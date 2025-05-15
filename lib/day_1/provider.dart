375b13072de41e287eba343b6e89c46309d9cc03
B3g1n3r <eswaransiva487@gmail.com>
2025-05-15 13:49:01 +0530

    working on provider

--- lib/day_1/provider.dart
+++ lib/day_1/provider.dart
@@ -1,16 +1,17 @@
-import 'package:flutter/material.dart';
 import 'package:daily_tasks/day_1/themes.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   bool isDarkTheme = true;
-   ThemeData? _themeData;
+  late ThemeData _themeData;
 
   ThemeProvider() {
-    _loadTheme(); // async function to load theme from SharedPreferences
+    _themeData = darkTheme; // prevent late init error
+    _loadTheme();
   }
 
-  ThemeData get themeMode => _themeData ?? lightTheme;
+  ThemeData get themeMode => _themeData;
 
   Future<void> _loadTheme() async {
     SharedPreferences pref = await SharedPreferences.getInstance();
