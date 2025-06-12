972169bc230632f1bd301b499f8f732da2c30fe3
B3g1n3r <eswaransiva487@gmail.com>
2025-06-12 11:58:26 +0530

    working on offline data

--- lib/day_6/notespage.dart
+++ lib/day_6/notespage.dart
@@ -1,6 +1,7 @@
 import 'package:daily_tasks/day_5/database.dart';
 import 'package:daily_tasks/day_6/notesview.dart';
 import 'package:daily_tasks/day_7/notificationService.dart';
+import 'package:daily_tasks/day_8/offline.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -20,88 +21,122 @@ class _NotespageState extends State<Notespage> {
     // TODO: implement initState
     super.initState();
     Notificationservice().initialize();
+    Offline().setNotes();
+    getList();
   }
+
+  List<String?> notesList = [];
+
+  void getList() async{
+    final list = await Offline().getNotes();
+    notesList = list ?? [];
+    setState(() {
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print('user $user');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Notes'),
-        actions: [IconButton(onPressed: (){
-          Notificationservice().sheduleNotification(context);
-        }, icon: Icon(Icons.message))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                Notificationservice().sheduleNotification(context);
+              },
+              icon: const Icon(Icons.message))
+        ],
       ),
       body: Column(
         children: [
+          // Expanded(
+          //   child: StreamBuilder(
+          //       stream: Database().getNotes(user),
+          //       builder: (context, snap) {
+          //         final snapData = snap.data;
+          //         if (!snap.hasData || snapData == null || snapData.isEmpty) {
+          //           return const Center(
+          //             child: Text('Add your notes'),
+          //           );
+          //         } else {
+          //           return GridView.builder(
+          //             itemCount: snapData.length,
+          //             itemBuilder: (context, index) {
+          //               final doc = snapData[index];
+          //               final String content = doc['content'] ?? 'no notes';
+          //               return InkWell(
+          //                 onTap: () {
+          //                   Navigator.push(
+          //                       context,
+          //                       PageRouteBuilder(
+          //                           transitionDuration:
+          //                               const Duration(seconds: 2),
+          //                           transitionsBuilder: (context, animation,
+          //                               secondaryAnimation, child) {
+          //                             const begin = Offset(1, 0);
+          //                             const end = Offset.zero;
+          //                             final curve = CurvedAnimation(
+          //                                 parent: animation,
+          //                                 curve: Curves.easeInCubic);
+          //                             final tween =
+          //                                 Tween(begin: begin, end: end);
+          //                             // return FadeTransition(opacity: animation, child: child,);
+          //                             return SlideTransition(
+          //                               position: tween.animate(curve),
+          //                               child: child,
+          //                             );
+          //                           },
+          //                           pageBuilder: (context, ani1, ani2) =>
+          //                               Notesview(
+          //                                   uid: user,
+          //                                   notesId: snapData[index].id,
+          //                                   notes: content)));
+          //                 },
+          //                 child: GestureDetector(
+          //                   onLongPress: () {
+          //                     Database().deleteNotes(user, snapData[index].id);
+          //                     Notificationservice().showNotification(doc['content'] ??'no data');
+          //                   },
+          //                   child: Card(
+          //                       color: Colors.greenAccent,
+          //                       child: Center(
+          //                           child: Text(
+          //                         content,
+          //                         maxLines: 3,
+          //                         overflow: TextOverflow.ellipsis,
+          //                       ))),
+          //                 ),
+          //               );
+          //             },
+          //             gridDelegate:
+          //                 const SliverGridDelegateWithFixedCrossAxisCount(
+          //               mainAxisSpacing: 10,
+          //               crossAxisSpacing: 10,
+          //               childAspectRatio: 3 / 2,
+          //               crossAxisCount: 2,
+          //             ),
+          //           );
+          //         }
+          //       }),
+          // ),
+
           Expanded(
-            child: StreamBuilder(
-                stream: Database().getNotes(user),
-                builder: (context, snap) {
-                  final snapData = snap.data;
-                  if (!snap.hasData || snapData == null || snapData.isEmpty) {
-                    return const Center(
-                      child: Text('Add your notes'),
-                    );
-                  } else {
-                    return GridView.builder(
-                      itemCount: snapData.length,
-                      itemBuilder: (context, index) {
-                        final doc = snapData[index];
-                        final String content = doc['content'] ?? 'no notes';
-                        return InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                PageRouteBuilder(
-                                    transitionDuration:
-                                        const Duration(seconds: 2),
-                                    transitionsBuilder: (context, animation,
-                                        secondaryAnimation, child) {
-                                      const begin = Offset(1, 0);
-                                      const end = Offset.zero;
-                                      final curve = CurvedAnimation(
-                                          parent: animation,
-                                          curve: Curves.easeInCubic);
-                                      final tween =
-                                          Tween(begin: begin, end: end);
-                                      // return FadeTransition(opacity: animation, child: child,);
-                                      return SlideTransition(
-                                        position: tween.animate(curve),
-                                        child: child,
-                                      );
-                                    },
-                                    pageBuilder: (context, ani1, ani2) =>
-                                        Notesview(
-                                            uid: user,
-                                            notesId: snapData[index].id,
-                                            notes: content)));
-                          },
-                          child: GestureDetector(
-                            onLongPress: () {
-                              Database().deleteNotes(user, snapData[index].id);
-                              Notificationservice().showNotification(doc['content'] ??'no data');
-                            },
-                            child: Card(
-                                color: Colors.greenAccent,
-                                child: Center(
-                                    child: Text(
-                                  content,
-                                  maxLines: 3,
-                                  overflow: TextOverflow.ellipsis,
-                                ))),
-                          ),
-                        );
-                      },
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        mainAxisSpacing: 10,
-                        crossAxisSpacing: 10,
-                        childAspectRatio: 3 / 2,
-                        crossAxisCount: 2,
-                      ),
-                    );
-                  }
-                }),
+            child: notesList.isEmpty
+                ? const Center(
+                    child: Text('no data in the list'),
+                  )
+                : ListView.builder(
+                    itemCount: notesList.length,
+                    itemBuilder: (context, index) {
+                      print('not empty');
+                      return Card(
+                        child: ListTile(
+                          title: Text(notesList[index] ?? 'No value'),
+                        ),
+                      );
+                    }),
           ),
           Align(
             alignment: Alignment.bottomCenter,
--- lib/day_8/offline.dart
+++ lib/day_8/offline.dart
@@ -1,5 +1,26 @@
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Offline {
-  
+  final user = FirebaseAuth.instance.currentUser!.uid;
+
+  Future<void> setNotes() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> notes = [];
+    final snapshot = await FirebaseFirestore.instance
+        .collection('Allnotes')
+        .doc(user)
+        .collection('notes').get();
+    for (var doc in snapshot.docs) {
+      notes.add(doc['content']);
+    }
+
+    await prefs.setStringList('notesList', notes); // consistent key
+  }
+
+  Future<List<String>?> getNotes() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList('notesList'); // same key
+  }
 }
