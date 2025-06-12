d8dac2a35628ba50cf48616d249d878b9218a5bc
B3g1n3r <eswaransiva487@gmail.com>
2025-06-12 12:08:26 +0530

    working on offline data

--- lib/day_6/notespage.dart
+++ lib/day_6/notespage.dart
@@ -51,76 +51,76 @@ class _NotespageState extends State<Notespage> {
       ),
       body: Column(
         children: [
-          // Expanded(
-          //   child: StreamBuilder(
-          //       stream: Database().getNotes(user),
-          //       builder: (context, snap) {
-          //         final snapData = snap.data;
-          //         if (!snap.hasData || snapData == null || snapData.isEmpty) {
-          //           return const Center(
-          //             child: Text('Add your notes'),
-          //           );
-          //         } else {
-          //           return GridView.builder(
-          //             itemCount: snapData.length,
-          //             itemBuilder: (context, index) {
-          //               final doc = snapData[index];
-          //               final String content = doc['content'] ?? 'no notes';
-          //               return InkWell(
-          //                 onTap: () {
-          //                   Navigator.push(
-          //                       context,
-          //                       PageRouteBuilder(
-          //                           transitionDuration:
-          //                               const Duration(seconds: 2),
-          //                           transitionsBuilder: (context, animation,
-          //                               secondaryAnimation, child) {
-          //                             const begin = Offset(1, 0);
-          //                             const end = Offset.zero;
-          //                             final curve = CurvedAnimation(
-          //                                 parent: animation,
-          //                                 curve: Curves.easeInCubic);
-          //                             final tween =
-          //                                 Tween(begin: begin, end: end);
-          //                             // return FadeTransition(opacity: animation, child: child,);
-          //                             return SlideTransition(
-          //                               position: tween.animate(curve),
-          //                               child: child,
-          //                             );
-          //                           },
-          //                           pageBuilder: (context, ani1, ani2) =>
-          //                               Notesview(
-          //                                   uid: user,
-          //                                   notesId: snapData[index].id,
-          //                                   notes: content)));
-          //                 },
-          //                 child: GestureDetector(
-          //                   onLongPress: () {
-          //                     Database().deleteNotes(user, snapData[index].id);
-          //                     Notificationservice().showNotification(doc['content'] ??'no data');
-          //                   },
-          //                   child: Card(
-          //                       color: Colors.greenAccent,
-          //                       child: Center(
-          //                           child: Text(
-          //                         content,
-          //                         maxLines: 3,
-          //                         overflow: TextOverflow.ellipsis,
-          //                       ))),
-          //                 ),
-          //               );
-          //             },
-          //             gridDelegate:
-          //                 const SliverGridDelegateWithFixedCrossAxisCount(
-          //               mainAxisSpacing: 10,
-          //               crossAxisSpacing: 10,
-          //               childAspectRatio: 3 / 2,
-          //               crossAxisCount: 2,
-          //             ),
-          //           );
-          //         }
-          //       }),
-          // ),
+          Expanded(
+            child: StreamBuilder(
+                stream: Database().getNotes(user),
+                builder: (context, snap) {
+                  final snapData = snap.data;
+                  if (!snap.hasData || snapData == null || snapData.isEmpty) {
+                    return const Center(
+                      child: Text('Add your notes'),
+                    );
+                  } else {
+                    return GridView.builder(
+                      itemCount: snapData.length,
+                      itemBuilder: (context, index) {
+                        final doc = snapData[index];
+                        final String content = doc['content'] ?? 'no notes';
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                    transitionDuration:
+                                        const Duration(seconds: 2),
+                                    transitionsBuilder: (context, animation,
+                                        secondaryAnimation, child) {
+                                      const begin = Offset(1, 0);
+                                      const end = Offset.zero;
+                                      final curve = CurvedAnimation(
+                                          parent: animation,
+                                          curve: Curves.easeInCubic);
+                                      final tween =
+                                          Tween(begin: begin, end: end);
+                                      // return FadeTransition(opacity: animation, child: child,);
+                                      return SlideTransition(
+                                        position: tween.animate(curve),
+                                        child: child,
+                                      );
+                                    },
+                                    pageBuilder: (context, ani1, ani2) =>
+                                        Notesview(
+                                            uid: user,
+                                            notesId: snapData[index].id,
+                                            notes: content)));
+                          },
+                          child: GestureDetector(
+                            onLongPress: () {
+                              Database().deleteNotes(user, snapData[index].id);
+                              Notificationservice().showNotification(doc['content'] ??'no data');
+                            },
+                            child: Card(
+                                color: Colors.greenAccent,
+                                child: Center(
+                                    child: Text(
+                                  content,
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                ))),
+                          ),
+                        );
+                      },
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                        childAspectRatio: 3 / 2,
+                        crossAxisCount: 2,
+                      ),
+                    );
+                  }
+                }),
+          ),
 
           Expanded(
             child: notesList.isEmpty
